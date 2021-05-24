bb09146615bbc9bdada099a40a19a518509832d5
Aditya <aditya201551@gmail.com>
2021-05-24 22:53:57 +0530

    added add and delete vertex

--- src/Graph.java
+++ src/Graph.java
@@ -18,8 +18,37 @@ public class Graph {
         }
 //        System.out.println(graph.get(u));
 //        System.out.println(graph.get(v));
-        graph.get(u).remove(new Integer(v));
-        graph.get(v).remove(new Integer(u));
+        graph.get(u).remove(v);
+        graph.get(v).remove(u);
+    }
+
+    void addVertex(ArrayList<HashSet<Integer>> graph)
+    {
+        graph.add(new HashSet<Integer>());
+
+    }
+
+    //This function is working fine but during printing the remaining vertex is taking the deleted vertex's place
+    void deleteVertex(ArrayList<HashSet<Integer>> graph, int vertex)
+    {
+        if(vertex>graph.size() || vertex<0)
+        {
+            System.out.println("Vertex you want to delete does not exist: enter the index of the vertex(starting from 0)");
+            return;
+        }
+        int i=0;
+        while(i!=vertex)
+            i++;
+//        System.out.println(i);
+        graph.remove(i);
+        for(HashSet<Integer> set:graph)
+        {
+            if(set.contains(vertex))
+            {
+                set.remove(vertex);
+                break;
+            }
+        }
     }
 
     void printGraph(ArrayList<HashSet<Integer>> graph)
@@ -42,10 +71,9 @@ public class Graph {
         ArrayList<HashSet<Integer>> graph= new ArrayList<>();
         int[][] node ={{0,1},{0,4},{1,2},{1,3},{1,4},{2,3},{3,4}};
         int numberOFNodes=5;
-
-        //initializing empty ArrayList with empty nested ArrayList inside it
+        //initializing empty ArrayList with empty nested HashSet inside
         for(int i=0;i<numberOFNodes;i++)
-            graph.add(new HashSet<Integer>());
+            ob.addVertex(graph);
 
         for(int[] i :node)
             ob.addEdge(graph,i[0],i[1]);
@@ -53,6 +81,8 @@ public class Graph {
         ob.printGraph(graph);
         ob.addEdge(graph,1,4);
         ob.deleteEdge(graph,1,4);
+//        ob.addVertex(graph);
+        ob.deleteVertex(graph,1);
         ob.printGraph(graph);
     }
 }
