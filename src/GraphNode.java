4151b50ebba4856bac9df78852928bb2f9115182
Aditya <aditya201551@gmail.com>
2021-05-26 13:54:49 +0530

    graph working

--- src/GraphNode.java
+++ src/GraphNode.java
@@ -1,58 +1,53 @@
 import java.util.*;
-class GraphNode
-{
-    int size=0;
-    class vertex
-    {
+class GraphNode {
+    int size = 0;
+
+    class vertex {
         vertex next;
         HashSet<vertex> edge;
         int weight;
-        vertex(int weight)
-        {
+
+        vertex(int weight) {
             this.weight = weight;
-            next=null;
-            edge=new HashSet<>();
+            next = null;
+            edge = new HashSet<>();
         }
     }
 
-    vertex head=null;
+    vertex head = null;
 
-    void addVertex(int weight)
-    {
+    void addVertex(int weight) {
         size++;
-        if(head==null) {
+        if (head == null) {
             head = new vertex(weight);
             return;
         }
-        vertex tmp=head;
-        while(tmp.next!=null)
-            tmp=tmp.next;
-        tmp.next=new vertex(weight);
+        vertex tmp = head;
+        while (tmp.next != null)
+            tmp = tmp.next;
+        tmp.next = new vertex(weight);
     }
 
-    void addEdge(int u, int v)
-    {
-        if(head==null) {
+    void addEdge(int u, int v) {
+        if (head == null) {
             System.out.println("Graph is empty");
             return;
         }
-        vertex originVertex=head;
-        while(originVertex!=null && originVertex.weight!=u)
-            originVertex=originVertex.next;
+        vertex originVertex = head;
+        while (originVertex != null && originVertex.weight != u)
+            originVertex = originVertex.next;
 
-        if(originVertex==null)
-        {
+        if (originVertex == null) {
             System.out.println("Cannot connect edge: INVALID vertex: INVALID origin vertex(u)");
             return;
         }
 
 
-        vertex destVertex=head;
-        while(destVertex!=null && destVertex.weight!=v)
-            destVertex=destVertex.next;
+        vertex destVertex = head;
+        while (destVertex != null && destVertex.weight != v)
+            destVertex = destVertex.next;
 
-        if(destVertex==null)
-        {
+        if (destVertex == null) {
             System.out.println("Cannot connect edge: INVALID vertex: INVALID destination vertex(v)");
             return;
         }
@@ -60,10 +55,8 @@ class GraphNode
         destVertex.edge.add(originVertex);
     }
 
-    void deleteVertex(int vertex)
-    {
-        if(head==null || (vertex<0 || vertex>size))
-        {
+    void deleteVertex(int vertex) {
+        if (head == null || (vertex < 0 || vertex > size)) {
             System.out.println("Graph is empty: Cannot delete an empty graph | INVALID vertex");
             return;
         }
@@ -72,13 +65,12 @@ class GraphNode
         vertex temp = head;
 
 
-        if (vertex == 0)
-        {
+        if (vertex == 0) {
             head = temp.next;   // Change head
             return;
         }
 
-        for (int i=0; temp!=null && i<vertex-1; i++)
+        for (int i = 0; temp != null && i < vertex - 1; i++)
             temp = temp.next;
 
 
@@ -87,40 +79,36 @@ class GraphNode
 
 
         //scanning every node and deleting the edge connected from this vertex
-        vertex v=head;
-        while(v!=null)
-        {
-            if(v.edge.contains(temp.next))
+        vertex v = head;
+        while (v != null) {
+            if (v.edge.contains(temp.next))
                 v.edge.remove(temp.next);
-            v=v.next;
+            v = v.next;
         }
 
         temp.next = temp.next.next;
     }
 
-    void deleteEdge(int u, int v)
-    {
-        if(head==null) {
+    void deleteEdge(int u, int v) {
+        if (head == null) {
             System.out.println("Graph is empty");
             return;
         }
-        vertex originVertex=head;
-        while(originVertex!=null && originVertex.weight!=u)
-            originVertex=originVertex.next;
+        vertex originVertex = head;
+        while (originVertex != null && originVertex.weight != u)
+            originVertex = originVertex.next;
 
-        if(originVertex==null)
-        {
+        if (originVertex == null) {
             System.out.println("Cannot connect edge: INVALID vertex: INVALID origin vertex(u)");
             return;
         }
 
 
-        vertex destVertex=head;
-        while(destVertex!=null && destVertex.weight!=v)
-            destVertex=destVertex.next;
+        vertex destVertex = head;
+        while (destVertex != null && destVertex.weight != v)
+            destVertex = destVertex.next;
 
-        if(destVertex==null)
-        {
+        if (destVertex == null) {
             System.out.println("Cannot connect edge: INVALID vertex: INVALID destination vertex(v)");
             return;
         }
@@ -129,4 +117,48 @@ class GraphNode
         destVertex.edge.remove(originVertex);
     }
 
+    void print()
+    {
+        if(head==null)
+        {
+            System.out.println("Empty graph: Cannot print");
+            return;
+        }
+        vertex tmp=head;
+        while(tmp!=null)
+        {
+            System.out.print(tmp.weight+"->");
+            Iterator<vertex> i=tmp.edge.iterator();
+            while(i.hasNext())
+                System.out.print(i.next().weight+",");
+            tmp=tmp.next;
+            System.out.println();
+        }
+        System.out.println("___________________________________________");
+    }
+
+    void checkConnections()
+    {
+        vertex tmp=head;
+        while(tmp!=null)
+        {
+            System.out.print(tmp.weight+" ");
+            tmp=tmp.next;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        GraphNode ob=new GraphNode();
+//        ob.print();
+        for(int i=0;i<5;i++)
+            ob.addVertex(i);
+        int[][] node = {{0, 1}, {0, 4}, {1, 2}, {1, 3}, {1, 4}, {2, 3}, {3, 4}};
+        for (int[] i : node)
+            ob.addEdge(i[0], i[1]);
+        ob.print();
+        ob.deleteVertex(4);
+        ob.print();
+//        ob.checkConnections();
+    }
 }
